0d269d8419cffc9bfdab93911785d23659b0ba2b
infinitybrad <seoih415@gmail.com>
2019-06-17 14:47:45 +0900

    first req

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use('/',(req,res) => {
+    res.status(200).json({
+        msg:'it Works'
+    });
+
+} );
 
 const server = http.createServer(app);
 
