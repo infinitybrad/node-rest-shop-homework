02a8c7a3ea4466116f40b2ebbd21cc314ba7d3fc
infinitybrad <seoih415@gmail.com>
2019-06-17 16:35:02 +0900

    add nodemon

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router =express.Router();
 
+// data get 
 router.get('/',(req,res) => {
     res.status(200).json({
         msg:'sucess get orders'
+    });
+});
+
+// data create
+router.post('/',(req,res) => {
+    res.status(200).json({
+        msg:'sucess post orders'
+    });
+});
+
+
+// data patch
+router.patch('/',(req,res) => {
+    res.status(200).json({
+        msg:'patch orders'
+    });
+});
+
+
+// data delete
+router.delete('/',(req,res) => {
+    res.status(200).json({
+        msg:'sucess delete orders'
     });
 });
 
