0815d82b2ffcdd2c46965ac1d3976d6c0fd8a876
infinitybrad <seoih415@gmail.com>
2019-06-17 16:38:43 +0900

    add morgan

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const morgan = require('morgan');
 
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products',productRoutes);
 app.use('/orders',ordersRoutes);
