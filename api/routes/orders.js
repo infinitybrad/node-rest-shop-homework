0d07fe3d8c5af17bf0bf8093d0822388cc242d7a
infinitybrad <seoih415@gmail.com>
2019-06-26 23:30:21 +0900

    add order post

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router =express.Router();
+const mongoose = require("mongoose");
+
+const orderModel= require("../models/order");
+const productModel = require("../models/product");
+
 
 // data get 
 router.get('/',(req,res) => {
@@ -10,9 +15,47 @@ router.get('/',(req,res) => {
 
 // data create
 router.post('/',(req,res) => {
-    res.status(200).json({
-        msg:'sucess post orders'
-    });
+    // res.status(200).json({
+    //     msg:'sucess post orders'
+    // });
+
+    productModel
+        .findById(req.body.productId)
+        .then(product => {
+            if(!product)
+            {
+                return res.status(404).json({
+                    msg:"product not found"
+                });
+            }
+            const order = new orderModel({
+                _id:mongoose.Types.ObjectId(),
+                product:req.body.productId,
+                quantity:req.body.quantity
+            });
+            return order.save();
+
+        })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                msg:"order stored",
+                createOrder:{
+                    _id:result.id,
+                    product:result.product,
+                    quantity:result.quantity
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
+
+
+
 });
 
 
