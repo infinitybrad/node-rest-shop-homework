edf48ef63a92ece6df932a6b69ed38db652d53b2
infinitybrad <seoih415@gmail.com>
2019-06-27 00:15:52 +0900

    add delete & detail

--- api/models/order.js
+++ api/models/order.js
@@ -4,10 +4,9 @@ const orderScheme = mongoose.Schema({
 
     _id:mongoose.Schema.ObjectId,
     product:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'product',
-        require:true
-
+        type:mongoose.Schema.Types.ObjectId, //'product'의 데이터 타입은 몽고디비의 아이디 형식이다.
+        ref:'product', //아이디 참조는 내가 만든 몽고디비의 'product' 컬렉션에서 한다.
+        require:true   //유저가 서버에 요청시 'product' 항목은 반드시 기입해야 한다.
     },
     quantity:{
         type:Number,
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -30,6 +30,38 @@ router.get('/',(req,res) => {
 
 });
 
+//get detail data
+router.get('/:orderID',(req,res) => { // 장바구니 한개의 데이터 불러오기
+
+    const id =  req.params.orderID;
+
+    orderModel
+        .findById(id)
+        .exec()
+        .then(result => {
+
+            if(!result)
+            {
+                return res.status(404).json({
+                    msg:"no id"
+                });
+            }
+            res.status(200).json({
+                msg:"successful order detail info ",
+                orderInfo:result
+            });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
+
+
+
+});
 // data create
 router.post('/',(req,res) => { // 장바구니에 제품 담기.
     // res.status(200).json({
@@ -84,11 +116,30 @@ router.patch('/',(req,res) => {
 });
 
 
-// data delete
-router.delete('/',(req,res) => {
-    res.status(200).json({
-        msg:'sucess delete orders'
-    });
+//detail data delete
+router.delete('/:orderID',(req,res) => {
+    // res.status(200).json({
+    //     msg:'sucess delete orders'
+    // });
+    const id  = req.params.orderID;
+
+    orderModel
+        .remove({_id:id})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                msg:"deleted order"
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
+
+
+
 });
 
 
