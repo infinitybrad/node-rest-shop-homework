47f8fc821d2b24bb5dd561e17227875b0f6f7427
infinitybrad <seoih415@gmail.com>
2019-06-28 19:09:10 +0900

    add bcrypt with password

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,34 +1,67 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 
 const userModel = require("../models/user");
 
 router.post('/signup',(req,res) => {
 
-    const user = new userModel({
-        _id:new mongoose.Types.ObjectId(),
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password
-    });
+    // const user = new userModel({
+    //     _id:new mongoose.Types.ObjectId(),
+    //     username:req.body.username,
+    //     email:req.body.email,
+    //     password:req.body.password
+    // });
 
-    user
-        .save()
-        .then(result => {
-            res.status(200).json({
-                msg:"user created",
-                userinfo:result
+    // user
+    //     .save()
+    //     .then(result => {
+    //         res.status(200).json({
+    //             msg:"user created",
+    //             userinfo:result
 
-            });
-        })
-        .catch(err =>  {
-            console.log(err);
-            res.status(500).json({
+    //         });
+    //     })
+    //     .catch(err =>  {
+    //         console.log(err);
+    //         res.status(500).json({
+    //             error:err
+
+    //         });
+    //     });
+
+    bcrypt.hash(req.body.password,10,(err,hash) => {
+
+        if(err)
+        {
+            return res.status(500).json({
                 error:err
-
             });
+        }
+        const user = new userModel({
+            _id: new mongoose.Types.ObjectId(),
+            username:req.body.username,
+            email:req.body.email,
+            password:hash
         });
+
+        user    
+            .save()
+            .then(result => {
+                res.status(200).json({
+                    msg:"user created",
+                    userinfo:result
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error:err
+                });
+            });
+    })
 
 });
 
